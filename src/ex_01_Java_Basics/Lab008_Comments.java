674beb720105248164fefc99378277f7f4fe3505
Rangarajan <rangu32@gmail.com>
2025-05-16 22:30:28 +0530

    "ex_01_Java_Basics" Done

---
+++ src/ex_01_Java_Basics/Lab008_Comments.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Comments {
+    /**
+     * Author - Rangarajan
+     * Course :  ATB12x
+     * This is a documentation comment
+     * @param args command-line arguments
+     * @return void
+     */
+    public static void main(String[] args) {
+        // Method implementation
+
+//        System.out.println("1");
+//        System.out.println("2");
+        System.out.println();
+    }
+}
---
+++ src/ex_01_Java_Basics/This_is_a_very_long_name_class_blah_blah.java
@@ -0,0 +1,5 @@
+package ex_01_Java_Basics;
+
+public class This_is_a_very_long_name_class_blah_blah {
+
+}
