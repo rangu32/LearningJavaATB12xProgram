16cd526bd99ffc4a0df7ab9058397d5181f2a0c4
Rangarajan <rangu32@gmail.com>
2025-06-29 15:47:02 +0530

    "21-06-2025" Object Class, Generics & Collection framework (List) - Done

---
+++ src/ex_26_Collection_Framework/LIST/Lab229_List.java
@@ -0,0 +1,14 @@
+package ex_26_Collection_Framework.LIST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab229_List {
+    public static void main(String[] args) {
+        List mylist = new ArrayList(5);
+        List mylist0 = new ArrayList();
+
+
+
+    }
+}
